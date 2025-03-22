eb6e62d9e67683eb70358eb2650a37d11b5f6d4e
waimea-lrdiack <lrdiack@waimea.school.nz>
2025-03-22 16:37:05 +1300

    Added coins

--- src/Main.kt
+++ src/Main.kt
@@ -28,6 +28,17 @@ fun main() {
     showGrids(grids)
     println()
 
+    println("Placing existing monkeys into cages...")
+
+    placeCoinInGrid(grids, 1, "Coin 1")
+    placeCoinInGrid(grids, 8, "Coin 2")
+    placeCoinInGrid(grids, 3, "Coin 3")
+    placeCoinInGrid(grids, 5, "Coin 4")
+    placeCoinInGrid(grids, 6, "Gold Coin")
+
+    println()
+
+    showGrids(grids)
 }
 
 fun setUpGrids(): MutableList<String> {
@@ -39,10 +50,20 @@ fun setUpGrids(): MutableList<String> {
 fun showGrids(gridList: List<String>) {
 
     // creates the divider line
-    val divider = "+--------".repeat(gridList.size) + "+"
+    val divider = "+-----------".repeat(gridList.size) + "+"
 
     println(divider)
-    for ((i, grid) in gridList.withIndex()) print("| ${grid.padEnd(6)} ")
+    for ((i, grid) in gridList.withIndex()) print("| ${grid.padEnd(8)} ")
     println("|")
     println(divider)
+}
+
+fun placeCoinInGrid(gridList: MutableList<String>, gridNum: Int, coin: String) {
+    // Check for invalid cage number
+    if (gridNum !in 1..NUMGRIDS) return
+    // Check for blank name
+    if (coin.isBlank()) return
+    // Ok to go ahead and place the monkey
+    println("+++ Putting $coin into grid $gridNum")
+    gridList[gridNum - 1] = coin
 }
